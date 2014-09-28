f661322f7c7a78aa90fa97d84353565a27eb0b65
AbdelmoulaAhmed <ahmed.abdelmoula@esprit.tn>
2014-09-28 18:18:30 +0200

    Project Init

---
+++ Assets/ScriptByAhmed/MoveJoystick.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var vitesseDeplacement=0.02;
+
+function Start () {
+
+}
+
+function Update () {
+//Test des touches du clavier
+if(Input){
+		rigidbody.AddForce(Input.GetAxis("Horizontal")* 10 * vitesseDeplacement * Time.deltaTime,0,0);
+	}else{
+	rigidbody.AddForce(0,0,0);
+	}
+	
+}
