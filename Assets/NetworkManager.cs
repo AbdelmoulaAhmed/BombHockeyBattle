59db688500c15d3067779581058da0c09f79c438
Mgarrech Yahia <mgarrechyahia92@gmail.com>
2014-09-29 21:28:32 +0100

    Multiplayer Connection Test
    Using Photon Plugin :
    - Connecting to a free server
    - Join the lobby
    - Jion a random room

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Connect ();
+	}
+
+	void Connect(){
+		PhotonNetwork.ConnectUsingSettings ("v1.0.0");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom (); 
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("Join Random Room Failed");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("Join New Room");
+	}
+}
--- Assets/ScriptByAhmed/MoveJoystick.js
+++ Assets/ScriptByAhmed/MoveJoystick.js
@@ -8,8 +8,8 @@ function Start () {
 function Update () {
 //Test des touches du clavier
 if(Input){
-		rigidbody.AddForce(Input.GetAxis("Horizontal")* 10 * vitesseDeplacement * Time.deltaTime,0,0);
-		rigidbody.AddForce(0,0,Input.GetAxis("Vertical")* 10 * vitesseDeplacement * Time.deltaTime);
+		rigidbody.AddForce(Input.GetAxis("Horizontal")* 1 * vitesseDeplacement * Time.deltaTime,0,0);
+		rigidbody.AddForce(0,0,Input.GetAxis("Vertical")* 1 * vitesseDeplacement * Time.deltaTime);
 		
 	}else{
 	rigidbody.AddForce(0,0,0);
